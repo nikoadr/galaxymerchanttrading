151edbb1e5281d01dd0cdeaeccfbf6b298eb519d
niko <niko.andrian@imaniprima.co.id>
2021-07-24 20:33:57 +0700

    update flowchart and updaate  database.sql

--- src/main/resources/database/database.sql
+++ src/main/resources/database/database.sql
@@ -1,85 +1,44 @@
-CREATE DATABASE dbtest;
+--create db
+CREATE DATABASE galaxymerchant;
 
-CREATE TABLE public.comodity (
-    id integer NOT NULL,
-    comodity_name character varying(50),
-    comodity_value double precision
-);
-
-
-ALTER TABLE public.comodity OWNER TO postgres;
-
---
--- TOC entry 205 (class 1259 OID 31000)
--- Name: comodity_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.comodity_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.comodity_id_seq OWNER TO postgres;
-
---
--- TOC entry 2937 (class 0 OID 0)
--- Dependencies: 205
--- Name: comodity_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.comodity_id_seq OWNED BY public.comodity.id;
-
-
---
--- TOC entry 2808 (class 2604 OID 31005)
--- Name: comodity id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.comodity ALTER COLUMN id SET DEFAULT nextval('public.comodity_id_seq'::regclass);
-
-
---
--- TOC entry 2931 (class 0 OID 31002)
--- Dependencies: 206
--- Data for Name: comodity; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.comodity (id, comodity_name, comodity_value) VALUES (2, 'Gold', 14450);
-INSERT INTO public.comodity (id, comodity_name, comodity_value) VALUES (3, 'Iron', 195.5);
-INSERT INTO public.comodity (id, comodity_name, comodity_value) VALUES (4, 'Silver', 17);
-
-
---
--- TOC entry 2938 (class 0 OID 0)
--- Dependencies: 205
--- Name: comodity_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.comodity_id_seq', 4, true);
-
+-- create roman_numeric_config table
 CREATE TABLE public.roman_numeric_config (
-    id integer DEFAULT nextval('public.roman_numeric_config_seq'::regclass) NOT NULL,
-    num_name character varying(50),
-    num_code character varying(50),
+    num_code character varying(1) NOT NULL,
     num_value integer
 );
 
-
 ALTER TABLE public.roman_numeric_config OWNER TO postgres;
+ALTER TABLE ONLY public.roman_numeric_config
+    ADD CONSTRAINT pk PRIMARY KEY (num_code);
 
---
--- TOC entry 2930 (class 0 OID 30984)
--- Dependencies: 204
--- Data for Name: roman_numeric_config; Type: TABLE DATA; Schema: public; Owner: postgres
---
+-- insert data to roman_numeri_config table
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('I', 1);
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('X', 10);
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('V', 5);
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('L', 50);
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('C', 100);
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('D', 500);
+INSERT INTO public.roman_numeric_config (num_code, num_value) VALUES ('M', 1000);
 
-INSERT INTO public.roman_numeric_config (id, num_name, num_code, num_value) VALUES (1, 'glob', 'I', 1);
-INSERT INTO public.roman_numeric_config (id, num_name, num_code, num_value) VALUES (2, 'prok', 'V', 5);
-INSERT INTO public.roman_numeric_config (id, num_name, num_code, num_value) VALUES (4, 'tegj', 'L', 50);
-INSERT INTO public.roman_numeric_config (id, num_name, num_code, num_value) VALUES (3, 'pish', 'X', 10);
+
+-- create intergalactic_unit_config table
+CREATE TABLE public.intergalactic_unit_config (
+    intergalactic_unit_name character varying(50),
+    roman_numeral character varying(1)
+);
+
+ALTER TABLE public.intergalactic_unit_config OWNER TO postgres;
+
+
+-- create comodity table
+CREATE TABLE public.comodity (
+    comodity_name character varying(100),
+    comodity_value double precision
+);
+
+ALTER TABLE public.comodity OWNER TO postgres;
+
+
+
 
 
